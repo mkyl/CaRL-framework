7bcd79cd2239c7fbc0adabca3a93bd6e60c60f0b
Moe Kayali <moe@kayali.io>
2020-01-08 23:26:40 -0800

    modelling step mockup begun

--- modeller/views.py
+++ modeller/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the modeller.")
+    template = loader.get_template('modeller/index.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
