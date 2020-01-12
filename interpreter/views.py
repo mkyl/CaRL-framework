26b7527b320fd8bbc02856ebc79f7c5025e78277
Moe Kayali <moe@kayali.io>
2020-01-11 17:47:40 -0800

    query evaluation mockup started

--- interpreter/views.py
+++ interpreter/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the interpeter.")
+    template = loader.get_template('interpreter/index.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
